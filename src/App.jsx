f39c0ec0e67a70d9b53b42f575970b5c6bdf83e3
DevLaren <larenthedev@gmail.com>
2024-09-24 13:36:18 +0100

    fixed router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -15,7 +15,7 @@ const App = () => {
         <Routes>
 
           <Route
-            index
+            exact path="/"
             element={<Home />}
           />
 
