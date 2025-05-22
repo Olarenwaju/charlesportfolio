1ab94976c68077429a76def794dfba551bc037cc
DevLaren <larenthedev@gmail.com>
2025-05-22 15:42:18 +0100

    new updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,16 +12,16 @@ const Contact = () => {
 
         <ul className="w-full sm:max-w-[450px] max-w-[277px] flex flex-row flex-wrap sm:gap-y-2 justify-between items-center  mx-auto  md:text-[25px] sm:text-[20px] text-[15px] font-normal sm:pt-15 pt-14 px-5">
 
-          <a href="https://www.linkedin.com/in/emmanuel-charles-8088b8205?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" rel="noopener noreferrer">
+          <a className="mr-1" href="https://www.linkedin.com/in/emmanuel-charles-8088b8205?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" rel="noopener noreferrer">
             <li className="underline underline-offset-2">LinkedIn</li>
           </a>
-          <a href="https://x.com/chizzyhemperor?s=21" target="_blank" rel="noopener noreferrer">
+          <a className="mr-1" href="https://x.com/chizzyhemperor?s=21" target="_blank" rel="noopener noreferrer">
             <li className="underline underline-offset-2">Twitter</li>
           </a>
-          <a href="https://www.instagram.com/designhemperor?igsh=eGR0d3FhOWY4dDh4&utm_source=qr" target="_blank" rel="noopener noreferrer">
+          <a className="mr-1" href="https://www.instagram.com/designhemperor?igsh=eGR0d3FhOWY4dDh4&utm_source=qr" target="_blank" rel="noopener noreferrer">
             <li className="underline underline-offset-2">Instagram</li>
           </a>
-          <a href="https://dribbble.com/DesignHemperor" target="_blank" rel="noopener noreferrer">
+          <a className="mr-1" href="https://dribbble.com/DesignHemperor" target="_blank" rel="noopener noreferrer">
             <li className="underline underline-offset-2">Dribble</li>
           </a>
 
